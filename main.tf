6e518b415516b1e27b3b18cc6f41d68f1e21459a
Jonathan Solórzano-Hamilton <jonathan.solorzano-hamilton@procore.com>
2018-12-31 16:37:39 -0800

    Revert "try to fix bad path"
    This reverts commit 0550e575b261919945b0b316552ea05086eebbb1.

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ data "archive_file" "notify_slack" {
   count = "${var.create}"
 
   type        = "zip"
-  source_file = "${path.module}/functions/notify_slack.py"
-  output_path = "${path.module}/functions/notify_slack.zip"
+  source_file = "${data.null_data_source.lambda_file.outputs.filename}"
+  output_path = "${data.null_data_source.lambda_archive.outputs.filename}"
 }
 
 resource "aws_lambda_function" "notify_slack" {
